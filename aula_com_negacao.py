dd0eb081329efa5693dea07308f0a64681f5c63c
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2023-02-09 11:06:05 -0300

    4 novas aulas de espressões regulares

---
+++ aula_com_negacao.py
@@ -0,0 +1,11 @@
+import re
+
+# Meta caracteres:
+# ^ -> começa com
+# $ -> termina com
+# [^a-z] -> Negação
+
+cpf = '147.852.963-12'
+print(re.findall(r'^((?:[0-9]{3}\.){2}[0-9]{3}-[0-9]{2})$', cpf))
+print(re.findall(r'[0-9^]+', cpf))
+print(re.findall(r'[^0-9a-zA-Z.-]+', cpf))
